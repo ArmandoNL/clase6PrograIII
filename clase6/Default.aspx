74eaee09b61e3111a19f649060b583f62b898f45
Armando Nevares <armandonl14@hotmail.com>
2017-02-21 12:16:53 -0600

    Listo, solo falta arreglar txtbox

--- clase6/Default.aspx
+++ clase6/Default.aspx
@@ -7,78 +7,78 @@
     <div class="jumbotron">
         <div class="row">
             <div class="col-md-9 col-md-push-3">
-                <input id="Text1" type="text" style="width:100%;"/>
+                <input id="txtVentana" runat="server" type="text" style="width:100%;"/>
             </div>
         </div>
 
         <div class="row">
             <div class="col-xs-6 col-sm-3">
-                <input id="Button1" type="button" value="Ans" onclick="" />
+                <input id="Button1" type="button" value="Ans" runat="server" onserverclick="ans" />
             </div>
             <div class="col-xs-6 col-sm-3">
-                <input id="Button2" type="button" value="C" />
+                <input id="Button2" type="button" value="C" runat="server" onserverclick="limpiar"/>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <input id="Button3" type="button" value="<--" /> 
+                <input id="Button3" type="button" value="<--" runat="server" onserverclick="borrar"/> 
             </div>
             <div class="col-xs-6 col-sm-3">
-                <input id="Button4" type="button" value="/" />
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-3">
-                <input id="Button5" type="button" value="7" />
-            </div>
-            <div class="col-xs-6 col-sm-3">
-                <input id="Button6" type="button" value="8" />
-            </div>
-            <div class="col-xs-6 col-sm-3">
-                <input id="Button7" type="button" value="9" />
-            </div>
-            <div class="col-xs-6 col-sm-3">
-                <input id="Button8" type="button" value="x" />
+                <input id="Button4" type="button" value="/" runat="server" onserverclick="dividir"/>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-6 col-sm-3">
-                <input id="Button9" type="button" value="4" />
+                <input id="Button5" type="button" value="7" runat="server" onserverclick="siete"/>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <input id="Button10" type="button" value="5" />
+                <input id="Button6" type="button" value="8" runat="server" onserverclick="ocho"/>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <input id="Button11" type="button" value="6" />
+                <input id="Button7" type="button" value="9" runat="server" onserverclick="nueve"/>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <input id="Button12" type="button" value="-" />
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-3">
-                <input id="Button13" type="button" value="1" />
-            </div>
-            <div class="col-xs-6 col-sm-3">
-                <input id="Button14" type="button" value="2" />
-            </div>
-            <div class="col-xs-6 col-sm-3">
-                <input id="Button15" type="button" value="3" />
-            </div>
-            <div class="col-xs-6 col-sm-3">
-                <input id="Button16" type="button" value="+" />
+                <input id="Button8" type="button" value="x" runat="server" onserverclick="multiplicar"/>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-6 col-sm-3">
-                <input id="Button17" type="button" value="-" />
+                <input id="Button9" type="button" value="4" runat="server" onserverclick="cuatro"/>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <input id="Button18" type="button" value="0" />
+                <input id="Button10" type="button" value="5" runat="server" onserverclick="cinco"/>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <input id="Button19" type="button" value="." />
+                <input id="Button11" type="button" value="6" runat="server" onserverclick="seis"/>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <input id="Button20" type="button" value="=" />
+                <input id="Button12" type="button" value="-" runat="server" onserverclick="restar"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-sm-3">
+                <input id="Button13" type="button" value="1" runat="server" onserverclick="uno"/>
+            </div>
+            <div class="col-xs-6 col-sm-3">
+                <input id="Button14" type="button" value="2" runat="server" onserverclick="dos"/>
+            </div>
+            <div class="col-xs-6 col-sm-3">
+                <input id="Button15" type="button" value="3" runat="server" onserverclick="tres"/>
+            </div>
+            <div class="col-xs-6 col-sm-3">
+                <input id="Button16" type="button" value="+" runat="server" onserverclick="sumar"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-sm-3">
+                <input id="Button17" type="button" value="-" runat="server" onserverclick="negativo"/>
+            </div>
+            <div class="col-xs-6 col-sm-3">
+                <input id="Button18" type="button" value="0" runat="server" onserverclick="cero"/>
+            </div>
+            <div class="col-xs-6 col-sm-3">
+                <input id="Button19" type="button" value="." runat="server" onserverclick="punto"/>
+            </div>
+            <div class="col-xs-6 col-sm-3">
+                <input id="Button20" type="button" value="=" runat="server" onserverclick="igual"/>
             </div>
         </div>
     </div>
--- clase6/Default.aspx.cs
+++ clase6/Default.aspx.cs
@@ -9,9 +9,216 @@ namespace clase6
 {
     public partial class _Default : Page
     {
+        static String operacion;
+        static int operador;
+        static int resAnterior;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                operacion = "";
+                operador = 0;
+                resAnterior = 0;
+            }
+        }
 
+        public void ans(object sender, EventArgs e)
+        {
+            operacion += resAnterior;
+            txtVentana.Value = operacion;
+        }
+
+        public void limpiar(object sender, EventArgs e)
+        {
+            operacion = "";
+            txtVentana.Value = operacion;
+        }
+
+        public void borrar(object sender, EventArgs e)
+        {
+            String aux = "";
+
+            for (int i = 0; i < operacion.Length - 1; i++)
+            {
+                aux += operacion.ElementAt(i);
+            }
+            operacion = aux;
+            txtVentana.Value = operacion;
+        }
+
+        public void dividir(object sender, EventArgs e)
+        {
+            //1
+            operador = 1;
+            operacion += '/';
+            txtVentana.Value = operacion;
+        }
+
+        public void multiplicar(object sender, EventArgs e)
+        {
+            //2
+            operador = 2;
+            operacion += 'x';
+            txtVentana.Value = operacion;
+        }
+
+        public void restar(object sender, EventArgs e)
+        {
+            //3
+            operador = 3;
+            operacion += '-';
+            txtVentana.Value = operacion;
+        }
+
+        public void sumar(object sender, EventArgs e)
+        {
+            //4
+            operador = 4;
+            operacion += '+';
+            txtVentana.Value = operacion;
+        }
+
+        public void igual(object sender, EventArgs e)
+        {
+            char[] separadores = { '-', '+', '/', 'x' };
+            String[] operandos = operacion.Split(separadores);
+            int resp = 0;
+            switch (operador)
+            {
+                case 1://division
+                    resp = int.Parse(operandos[0]);
+                    int i = 1;
+                    bool bandera = true;
+                    while (i < operandos.Length & bandera == true)
+                    {
+                        if (int.Parse(operandos[i]) != 0)
+                        {
+                            resp = resp / int.Parse(operandos[i]);
+                        }
+                        else
+                        {
+                            bandera = false;
+                        }
+                        i++;
+                    }
+                    if (bandera == true)
+                    {
+                        txtVentana.Value = resp.ToString();
+                        resAnterior = resp;
+                        operacion = resp.ToString();
+                    }
+                    else
+                    {
+                        txtVentana.Value = "Devisión por 0 no definida.";
+                        resAnterior = 0;
+
+                    }
+                    break;
+                case 2://multiplicacion
+                    resp = int.Parse(operandos[0]);
+                    for (i = 1; i < operandos.Length; i++)
+                    {
+                        resp = resp * int.Parse(operandos[i]);
+                    }
+                    txtVentana.Value = resp.ToString();
+                    resAnterior = resp;
+                    operacion = resp.ToString();
+                    break;
+                case 3://resta
+                    resp = int.Parse(operandos[0]);
+                    for (i = 1; i < operandos.Length; i++)
+                    {
+                        resp = resp - int.Parse(operandos[i]);
+                    }
+                    txtVentana.Value = resp.ToString();
+                    resAnterior = resp;
+                    operacion = resp.ToString();
+                    break;
+                case 4://suma
+                    resp = int.Parse(operandos[0]);
+                    for (i = 1; i < operandos.Length; i++)
+                    {
+                        resp = resp + int.Parse(operandos[i]);
+                    }
+                    txtVentana.Value = resp.ToString();
+                    resAnterior = resp;
+                    operacion = resp.ToString();
+                    break;
+
+            }
+        }
+
+        public void punto(object sender, EventArgs e)
+        {
+            operacion += '.';
+            txtVentana.Value = operacion;
+        }
+
+        public void uno(object sender, EventArgs e)
+        {
+            operacion += '1';
+            txtVentana.Value = operacion;
+        }
+
+        public void dos(object sender, EventArgs e)
+        {
+            operacion += '2';
+            txtVentana.Value = operacion;
+        }
+
+        public void tres(object sender, EventArgs e)
+        {
+            operacion += '3';
+            txtVentana.Value = operacion;
+        }
+
+        public void cuatro(object sender, EventArgs e)
+        {
+            operacion += '4';
+            txtVentana.Value = operacion;
+        }
+
+        public void cinco(object sender, EventArgs e)
+        {
+            operacion += '5';
+            txtVentana.Value = operacion;
+        }
+
+        public void seis(object sender, EventArgs e)
+        {
+            operacion += '6';
+            txtVentana.Value = operacion;
+        }
+
+        public void siete(object sender, EventArgs e)
+        {
+            operacion += '7';
+            txtVentana.Value = operacion;
+        }
+
+        public void ocho(object sender, EventArgs e)
+        {
+            operacion += '8';
+            txtVentana.Value = operacion;
+        }
+
+        public void nueve(object sender, EventArgs e)
+        {
+            operacion += '9';
+            txtVentana.Value = operacion;
+        }
+
+        public void cero(object sender, EventArgs e)
+        {
+            operacion += '0';
+            txtVentana.Value = operacion;
+        }
+
+        public void negativo(object sender, EventArgs e)
+        {
+            operacion += '-';
+            txtVentana.Value = operacion;
         }
     }
 }
